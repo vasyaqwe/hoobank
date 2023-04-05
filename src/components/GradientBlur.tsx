7eeacee386a225704b5f0bd35f4cc3c95397d8fd
vasyaqwe <elekton.fafik@gmail.com>
2023-04-05 14:39:06 +0300

    testing blurring

--- src/components/GradientBlur.tsx
+++ src/components/GradientBlur.tsx
@@ -2,9 +2,10 @@ import { motion } from 'framer-motion'
 
 export const GradientBlur = ({ className, animate = true }: { className: string, animate?: boolean }) => {
     return (
-        animate ? <motion.div animate={{ y: [0, 30, 30], x: [0, 50, 50], scale: [1, 1.2, 1], }}
-            transition={{ duration: 5, repeat: Infinity, repeatType: "reverse", }}
-            aria-hidden="true" className={`${className} w-[750px] blur-[325px] z-[-1] h-[550px] rounded-3xl absolute`} /> :
-            <div aria-hidden="true" className={`${className} w-[750px] blur-[325px] z-[-1] h-[550px] rounded-3xl absolute`} />
+        // animate ? <motion.div animate={{ y: [0, 30, 30], x: [0, 50, 50], scale: [1, 1.2, 1], }}
+        //     transition={{ duration: 5, repeat: Infinity, repeatType: "reverse", }}
+        //     aria-hidden="true" className={`${className} w-[750px] blur-[325px] z-[-1] h-[550px] rounded-3xl absolute`} /> :
+        //     <div aria-hidden="true" className={`${className} w-[750px] blur-[325px] z-[-1] h-[550px] rounded-3xl absolute`} />
+        <div></div>
     )
 }
